49c79d900da1254a0b7bfc6d281a4cc9577a04dc
Fredrik Jonsson <frjo@xdeb.org>
2007-11-19 07:32:27 +0000

    THis is for Drupal 5

--- ajax_vote_up_down.js
+++ ajax_vote_up_down.js
@@ -4,9 +4,10 @@ Drupal.voteUpDownAutoAttach = function() {
   var vdb = [];
   $('span.vote-up-inact, span.vote-down-inact, span.vote-up-act, span.vote-down-act').each(function () {
     // Read in the path to the PHP handler
-    uri = $(this).attr('title');
-    // Remove the title, so no tooltip will display
-    $(this).removeAttr('title');
+    var uri = $(this).attr('title');
+    // Get title from a tag.
+    var atitle = $(this).children('a').attr('title');
+    $(this).attr('title', atitle);
     // remove href link
     $(this).html('');
     // Create an object with this uri. Because
@@ -21,33 +22,33 @@ Drupal.voteUpDownAutoAttach = function() {
 /**
  * A Vote DataBase object
  */
-Drupal.VDB = function(elt, uri, id) {
+Drupal.VDB = function(elt, uri) {
   var db = this;
   // By making the span element a property of this object,
   // we get the ability to attach behaviours to that element.
   this.elt = elt;
   this.uri = uri;
   this.id = $(elt).attr('id');
+  this.type = this.id.indexOf('node') > -1 ? 'node' : 'comment';
   this.dir1 = this.id.indexOf('vote_up') > -1 ? 'up' : 'down';
   this.dir2 = this.dir1 == 'up' ? 'down' : 'up';
+  // Extract the cid so we can change other elements for the same cid
+  this.cid = db.id.match(/[0-9]+$/);
   $(elt).click(function() {
     // Ajax GET request for vote data
     $.ajax({
       type: "GET",
       url: db.uri,
       success: function (data) {
-        // extract the cid so we can change other elements for the same cid
-        var cid = db.id.match(/[0-9]+$/);
-        var pid = 'vote_points_' + cid;
         //update the voting arrows
         $('#' + db.id + '.vote-' + db.dir1 + '-inact')
           .removeClass('vote-' + db.dir1 + '-inact')
           .addClass('vote-' + db.dir1 + '-act');
-        $('#' + 'vote_' + db.dir2 + '_' + cid)
+        $('#vote_' + db.dir2 + '_' + db.type + '_' + db.cid)
           .removeClass('vote-' + db.dir2 + '-act')
           .addClass('vote-' + db.dir2 + '-inact');
         // update the points
-        $('#' + pid).html(data);
+        $('#vote_points_' + db.type + '_' + db.cid).html(data);
       },
       error: function (xmlhttp) {
         alert('An HTTP error '+ xmlhttp.status +' occured.\n'+ db.uri);
