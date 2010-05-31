5195fbadca27fccf75ac42e880b401b7504eeea2
Marco Villegas <marvil07@gmail.com>
2010-05-31 03:59:40 +0000

    syncing HEAD with DRUPAL-6--2
    now HEAD will be 6.x-3.x and 6.x-2.x is in the process of become
    stable(aka finally!)

---
+++ widgets/plain/votes.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+/**
+ * @file
+ * votes.tpl.php
+ *
+ * Plain widget votes display for Vote Up/Down
+ */
+?>
+<span id="<?php print $id; ?>" class="total-votes-plain"><span class="<?php print $class; ?> total"><?php print $unsigned_points; ?> <?php print $vote_label; ?></span></span>
